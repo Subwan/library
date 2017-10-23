010e03b312f17e80fb80f8f77c9982f8208d6bb4
Subwan <Subwan@gmail.com>
2017-10-23 23:56:48 +0300

    Create structure, constructor and get, set for Book.

---
+++ src/com/library/www/BL/Book.java
@@ -0,0 +1,49 @@
+package com.library.www.BL;
+
+import java.time.*;
+
+public class Book {
+    private int id;
+    private String name;
+    private LocalDateTime date;
+    private boolean availability;
+
+    public Book(int id, String name, LocalDateTime date, boolean availability) {
+        setId(id);
+        setName(name);
+        setDate(date);
+        setAvailability(availability);
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    private void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public boolean getAvailability() {
+        return availability;
+    }
+}
