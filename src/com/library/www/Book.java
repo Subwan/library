5c75e0140b8d45399c33e3d9aaafba3007956334
Subwan <Subwan@gmail.com>
2017-10-23 21:35:34 +0300

    Initial commit

---
+++ src/com/library/www/Book.java
@@ -0,0 +1,11 @@
+package com.library.www;
+
+import java.time.*;
+
+public class Book {
+    private int id;
+    private String name;
+    private LocalDateTime date;
+    private boolean availability;
+
+}
---
+++ src/com/library/www/Main.java
@@ -0,0 +1,8 @@
+package com.library.www;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
