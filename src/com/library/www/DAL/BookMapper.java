88cddb4af75e8a0f2a04ec412cdab0ff00271f82
alexandr <Subwan@gmail.com>
2017-10-24 10:35:52 +0300

    Create class BookMapper,  create empty override metods

---
+++ src/com/library/www/DAL/BookMapper.java
@@ -0,0 +1,36 @@
+package com.library.www.DAL;
+
+import com.library.www.BL.Book;
+
+import java.sql.ResultSet;
+
+/**
+ * Created by alexandr on 24.10.17.
+ */
+public class BookMapper extends AbstractMapper{
+
+    @Override
+    public Book findById(long id) {
+
+    }
+
+    @Override
+    public boolean insertBook(Book book){
+
+    }
+
+    @Override
+    public boolean updateBook(Book book){
+
+    }
+
+    @Override
+    public boolean deleteBook(long id){
+
+    }
+
+    @Override
+    protected Book doLoad(ResultSet result){
+
+    }
+}
