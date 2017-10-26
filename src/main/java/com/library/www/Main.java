d4fe1a723c21bea699840ae805da3ae59a29be2b
alexandr <Subwan@gmail.com>
2017-10-26 10:29:47 +0300

    Add test-servlet, index.html

--- src/main/java/com/library/www/Main.java
+++
@@ -1,8 +0,0 @@
-package com.library.www;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/library/www/Servlets/TestServlet.java
@@ -0,0 +1,10 @@
+package com.library.www.Servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by alexandr on 26.10.17.
+ */
+public class TestServlet extends HttpServlet {
+
+}
